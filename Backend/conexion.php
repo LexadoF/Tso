c64869a95dc6884bc62a075b6f7e4bd2c5263d88
Steve Fox <stevefoly@gmail.com>
2020-11-13 18:24:50 -0500

    Update conexion.php

--- Backend/conexion.php
+++ Backend/conexion.php
@@ -2,7 +2,7 @@
 
 // DATOS DE CONEXION A LA BASE DE DATOS
 function conexion() {
-  $conexion = mysqli_connect("localhost", "root", "", "pruebas");
+  $conexion = mysqli_connect("localhost", "root", "", "tso");
   
   return $conexion;
 }

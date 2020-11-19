b3c7aa1f1e89d8e5f997f0ed1ade43be6fa3a22b
LeonelBaena <leonelbaena2@gmail.com>
2020-11-19 17:27:25 -0500

    nuevos componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule} from './app-routing.module';
+
+
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ProductosComponent } from './components/productos/productos.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,

55646f5bb53acf3085d212910d9c52c655d84253
LeonelBaena <leonelbaena2@gmail.com>
2020-11-22 16:52:41 -0500

    recomienzo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { NoticiaComponent } from './components/noticia/noticia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent },
+  {path: 'nosotros', component: NosotrosComponent },
+  {path: 'contacto', component: ContactoComponent },
+  {path: 'noticia', component: NoticiaComponent },
+  {path: 'productos', component: ProductosComponent },
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { NoticiaComponent } from './components/noticia/noticia.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ChatComponent,
+    ContactoComponent,
+    NosotrosComponent,
+    NoticiaComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,

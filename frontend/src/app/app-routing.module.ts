424924e7bf029a1999df750919b955b998c1e6cb
Steve Fox <stevefoly@gmail.com>
2020-11-28 20:09:40 -0500

    Restart

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,25 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NosotrosComponent } from './components/nosotros/nosotros.component';
-import { ProductosComponent } from './components/productos/productos.component';
-import { NoticiaComponent } from './components/noticia/noticia.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistrarseComponent } from './components/registrarse/registrarse.component';
-import { AuthguardGuard } from './components/login/servicios/authguard.guard';
 
-const routes: Routes = [
-  {path: 'inicio', component: InicioComponent },
-  {path: 'login', component: LoginComponent },
-  {path: 'registro', component: RegistrarseComponent },
-  {path: 'nosotros', component: NosotrosComponent },
-  {path: 'contacto', component: ContactoComponent },
-  {path: 'noticia', component: NoticiaComponent, canActivate: [AuthguardGuard]  },
-  {path: 'productos', component: ProductosComponent },
-  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,54 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { NosotrosComponent } from './components/nosotros/nosotros.component';
-import { NoticiaComponent } from './components/noticia/noticia.component';
-import { ProductosComponent } from './components/productos/productos.component';
-import { RegistrarseComponent } from './components/registrarse/registrarse.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
-
-// Servicios
-import { RegistroService } from '../app/services/cliente/registro.service';
-import { ApiService } from '../app/components/login/servicios/serviciologin';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    InicioComponent,
-    ChatComponent,
-    ContactoComponent,
-    NosotrosComponent,
-    NoticiaComponent,
-    ProductosComponent,
-    RegistrarseComponent,
-    FooterComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [
-    RegistroService,
-    ApiService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

6e061d65b34fa47fa18dfcfcbf407ae5f7ef9f61
Steve Fox <stevefoly@gmail.com>
2020-12-04 13:49:50 -0500

    productos cambio

---
+++ Tienda/productos/templates/cabecera.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>carro</title>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery.bxSlider.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script> -->
+</head>
+<body>
+
+
+
+<header>
+<section class="header-top">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-4">
+                   
+                   </div>
+                <div class="col-md-4">
+                    <div class="icon">
+                       <a href="#"><i class="fa fa-facebook"></i></a>
+                       <a href="#"><i class="fa fa-twitter"></i></a>
+                       <a href="#"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                   <div class="a-right">
+                        <a href="/Tienda/login.php"><p><span><i class="fa fa-user"></i></span>Iniciar Sesión</p></a>
+                        <a href="/Tienda/registrarse.php"><p><span><i class="fa fa-pencil"></i></span>Registrarse</p></a>
+                   </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<div class="clear"></div>
+<section class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                   <div class="logo">
+                    <a href="/Tienda/index.php"><h2>TSO</h2></a>
+                   </div>
+                </div>
+                <div class="col-md-7">
+                    <ul class="nav navbar-nav">
+                      <li class="active"><a href="/Tienda/index.php">INICIO</a></li>
+                      <li><a href="/Tienda/nosotros.php">NOSOTROS</a></li>
+                      <li><a href="/Tienda/contacto.php">CONTACTO</a></li>
+                      <li><a href="/Tienda/noticias.php">NOTICIAS</a></li>
+                      <li><a href="/Tienda/productos.php">PRODUCTOS</a></li>
+                    </ul>
+                </div>
+                <li class="nav-item active">
+                    <a class="nav-link" href="mostrarCarrito.php">carrito (
+                        <?php
+                         echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']);
+                    
+                    ?> )</a>
+                </li>
+            </div>
+        </div>
+    </section>
+
+</header>
+<section class="header-feahion">
+    <img src="img/header-image.jpg" alt="">
+    <div class="header-text">
+        <h4>FREE SHIPPING ON ORDERS OVER $100!</h4>
+        <h3>HOT NEW RANGE <br> IN STOCK!</h3>
+    </div>
+</section>
+    <div class="container">
